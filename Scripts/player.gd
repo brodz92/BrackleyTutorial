dd91a156886e506153d43e844bea107e51b6406a
brodz92 <163666491+brodz92@users.noreply.github.com>
2024-05-07 01:32:06 +1000

    Update
    Cleaned Up Code, added Death Animation, Health Bar & Scene Reset

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,87 +1,84 @@
 extends CharacterBody2D
 
-var health
-var maxHealth = 3
+# Constants
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+# Exported and onready variables
+var maxHealth := 3
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var timer: Timer = $Timer
 
+# State variables
+var health
+var isDead = false
 
 func _ready():
-	#var sigBus = get_node("/root/SignalBus")
-	SignalBus.hurt_player.connect(on_hurt_player)
-	SignalBus.dead_player.connect(on_dead_player)
 	health = maxHealth
 	health_bar.max_value = maxHealth
 	health_bar.value = maxHealth
-	timer.wait_time = 0.5
-	
+	SignalBus.hurt_player.connect(on_hurt_player)  # Ensure SignalBus is a valid node path
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
- 
 func on_hurt_player():
 	print("Took Damage")
 	reduce_health()
-	
 
 func reduce_health():
-	health -= 1
-	health_bar.value = health
-	print("Health = " + str(health))
-	if health == 0:
-		SignalBus.emit_signal("dead_player")
+	if health > 0:
+		health -= 1
+		health_bar.value = health
+		print("Health = " + str(health))
+		if health == 0:
+			trigger_death()
 
-func on_dead_player():
-	timer.start()
-	Engine.time_scale = 0.5
+func trigger_death():
+	if !isDead:
+		isDead = true
+		timer.start()
+		Engine.time_scale = 0.5
+		animated_sprite.play("death")
 
-func _on_timer_timeout() -> void:
+func _on_timer_timeout():
 	print("You died!")
-	#get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	Engine.time_scale = 1
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	handle_gravity(delta)
+	handle_jump()
+	handle_movement(delta)
+
+func handle_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+func handle_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func handle_movement(delta):
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
+	handle_animation(direction)
+	if not isDead:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		velocity.x = move_toward(velocity.x, 0, 0)
+
+	move_and_slide()
+
+func handle_animation(direction: int):
+	animated_sprite.flip_h = direction < 0
+	if isDead:
+		return
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("Idle")
 		else:
 			animated_sprite.play("run")
-	elif !is_on_floor():
+	elif not is_on_floor():
 		animated_sprite.play("jump")
-	elif health == 0:
-		Engine.time_scale = 0.5
-		animated_sprite.play("death")
-	
-	
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-
-
-
-
-
-
